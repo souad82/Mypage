f71791ee50419c11e1a326865e8001c469f36fa2
souad82 <104201787+souad82@users.noreply.github.com>
2022-09-07 17:26:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let title = document.querySelector("title")
 title.innerHtml = "MyPage"
 
 let h1 = document.querySelector("h1")
-title.innerHtml = "c'est ok, pas de KHello WoRd"
+h1.innerHtml = "c'est ok, pas de KHello WoRd"
